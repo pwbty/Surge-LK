ad2a9439d7a8610513605a3fe5d2cee328b43daf
Matt <Geczy@users.noreply.github.com>
2023-11-08 11:16:53 -0600

    Create Mealime.js

---
+++ scripts/langkhach/Mealime.js
@@ -0,0 +1,5 @@
+let obj=JSON.parse($response.body)
+obj.subscription["created_at"] = 1699095873000;
+obj.subscription["is_active"] = true;
+obj.subscription["canceled_at"] = null;
+$done({body:JSON.stringify(obj)})
