702f293ed42eb89ea046e06743efc346e4320145
Matt <Geczy@users.noreply.github.com>
2023-11-08 12:04:09 -0600

    Update Mealime.js

--- scripts/langkhach/Mealime.js
+++ scripts/langkhach/Mealime.js
@@ -1,6 +1,7 @@
-let obj=JSON.parse($response.body)
-obj.subscription = {};
-obj.subscription["created_at"] = 1699095873000;
-obj.subscription["is_active"] = true;
-obj.subscription["canceled_at"] = null;
-$done({body:JSON.stringify(obj)})
+var obj = JSON.parse($response.body);
+obj.subscription = {
+  created_at: 1699095873000,
+  is_active: true,
+  canceled_at: null
+};
+$done({body: JSON.stringify(obj)});
